d1c9b18b560afd965a26f6bb1d64bc4b35590031
mikrozhenek <zheka0723@gmail.com>
2024-02-01 12:00:46 +0300

    some changess

--- sql/create-tables.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE transactions (
-    id SERIAL PRIMARY KEY,
-    wallet_id VARCHAR(255) NOT NULL,
-    amount FLOAT NOT NULL,
-    type VARCHAR(255) NOT NULL,
-    date_created TIMESTAMP,
-    status VARCHAR(255) NOT NULL
-);
